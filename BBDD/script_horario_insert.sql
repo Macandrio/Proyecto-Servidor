c40bb1d0a3c9606c1d8ab7e597123af85fc9d6b7
Alejandro gomez <alejandro.gomez.rivera.al@iespoligonosur.org>
2025-03-26 12:14:21 +0100

    script base de datos

--- BBDD/script_horario_insert.sql
+++ BBDD/script_horario_insert.sql
@@ -1,5 +1,13 @@
 
--- CREACIÓN DE TABLAS
+-- Eliminamos las tablas si existen (para evitar errores en pruebas sucesivas)
+DROP TABLE IF EXISTS Ausencia;
+DROP TABLE IF EXISTS Horario;
+DROP TABLE IF EXISTS Franja;
+DROP TABLE IF EXISTS Aula;
+DROP TABLE IF EXISTS Curso;
+DROP TABLE IF EXISTS Asignatura;
+DROP TABLE IF EXISTS Profesor;
+DROP TABLE IF EXISTS Usuario;
 
 -- Tabla Usuario
 CREATE TABLE Usuario (
